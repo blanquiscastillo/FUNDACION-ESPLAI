9e9bddaca30dccb4edaf730a1be5b2acc3deb283
blanquiscastillo <81380260+blanquiscastillo@users.noreply.github.com>
2021-05-05 11:56:47 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAREA01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 6;
+            double num3 = 2.57;
+            double suma;
+            suma = num1 + num2 + num3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+            Console.ReadKey();
+
+        }
+    }
+}
